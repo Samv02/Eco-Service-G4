a5b0f1b40b886113740e4667eceb736a03d196bb
Warzaros <65943109+Warzaros@users.noreply.github.com>
2023-12-19 15:47:52 +0100

    HomeController & home.twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-// Import necessary classes
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -29,4 +28,3 @@ class HomeController extends AbstractController
         ]);
     }
 }
-
